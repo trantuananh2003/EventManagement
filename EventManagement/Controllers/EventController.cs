cf9a1325286a6d207a0273951f5bf502f915c0ce
Tran Tuan Anh <tuananh2003.it@gmail.com>
2024-11-05 10:12:55 +0700

    route from EventController

--- EventManagement/Controllers/EventController.cs
+++ EventManagement/Controllers/EventController.cs
@@ -75,7 +75,7 @@ namespace EventManagement.Controllers
         }
 
         // Tạo mới event
-        [HttpPost("event")]
+        [HttpPost()]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
